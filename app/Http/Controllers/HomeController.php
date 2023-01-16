5ad6a4cadba9f5702b3529cb1fce400f5945205a
SantaCruz77 <steveyamada66@gmail.com>
2023-01-16 18:59:25 +0900

    develop create-function

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Memo;
 
 class HomeController extends Controller
 {
@@ -23,12 +24,37 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('home');
+        $user = \Auth::user();
+        // メモ一覧を取得
+        // ASC=昇順、DESC=降順
+        $memos = Memo::where('user_id', $user['id'])->
+                       where('status', 1)->
+                       orderBy('updated_at', 'DESC')->
+                       get();
+        return view('home', compact('user', 'memos'));
     }
     
     public function create()
     {
+        // ログインしているユーザー情報をViewに渡す
         $user = \Auth::user();
         return view('create', compact('user'));
+    }
+    
+    public function store(Request $request)
+    {
+        $data = $request->all();
+        // dd($data);
+        // POSTされたデータをDB（memosテーブル）に挿入
+        // MEMOモデルにDBへ保存する命令を出す
+
+        $memo_id = Memo::insertGetId([
+            'content' => $data['content'],
+            'user_id' => $data['user_id'], 
+            'status' => 1
+        ]);
+        
+        // リダイレクト処理
+        return redirect()->route('home');
     }
 }
