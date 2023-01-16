7caff44906ce981dfbad6dc80c3cc91dfc764586
SantaCruz77 <steveyamada66@gmail.com>
2023-01-16 22:41:35 +0900

    develop tag-function

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Memo;
+use App\Tag;
 
 class HomeController extends Controller
 {
@@ -38,7 +39,11 @@ class HomeController extends Controller
     {
         // ログインしているユーザー情報をViewに渡す
         $user = \Auth::user();
-        return view('create', compact('user'));
+        $memos = Memo::where('user_id', $user['id'])->
+                       where('status', 1)->
+                       orderBy('updated_at', 'DESC')->
+                       get();
+        return view('create', compact('user', 'memos'));
     }
     
     public function store(Request $request)
@@ -48,9 +53,15 @@ class HomeController extends Controller
         // POSTされたデータをDB（memosテーブル）に挿入
         // MEMOモデルにDBへ保存する命令を出す
 
+        // 先にタグをインサート
+        $tag_id = Tag::insertGetId(['name' => $data['tag'], 'user_id' => $data['user_id']]);
+        // dd($tag_id);
+        // タグのIDが判明する
+        // タグIDをmemosテーブルに入れてあげる
         $memo_id = Memo::insertGetId([
             'content' => $data['content'],
-            'user_id' => $data['user_id'], 
+            'user_id' => $data['user_id'],
+            'tag_id' =>$tag_id, 
             'status' => 1
         ]);
         
@@ -68,15 +79,16 @@ class HomeController extends Controller
                        where('status', 1)->
                        orderBy('updated_at', 'DESC')->
                        get();
-        //取得したメモをViewに渡す
-        return view('edit',compact('memo', 'user', 'memos'));
+        // 取得したメモをViewに渡す
+        $tags = Tag::where('user_id', $user['id'])->get();
+        return view('edit',compact('memo', 'user', 'memos', 'tags'));
     }
 
     public function update(Request $request, $id)
     {
         $inputs = $request->all();
         // dd($inputs);
-        Memo::where('id', $id)->update(['content' => $inputs['content'] ]);
+        Memo::where('id', $id)->update(['content' => $inputs['content'], 'tag_id' => $inputs['tag_id'] ]);
         return redirect()->route('home');
     }
 }
